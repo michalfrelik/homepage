9146272876b9c8c45c8df4f8f2ac29e5b1291a08
Michal Frelik <michal.frelik1@gmail.com>
2020-03-19 13:13:29 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Siema');
+const name = 'Michał Frelik';
+const age = 26;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age}  lat`);
+alert('Witam na stronie do nauki frontendu')
