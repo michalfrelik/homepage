596572abc77d2b061b736b1f26307b433791923e
Michal Frelik <michal.frelik1@gmail.com>
2020-03-25 21:37:29 +0100

    DOM - lesson

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const age = 26;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age}  lat`);
+
+const homepage__header = document.querySelector('.homepage__header');
+
+console.log(homepage__header);
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[1].innerHTML);
+
+
+const h1 = document.querySelector('h1');
+
+console.log(h1.innerHTML);
+
+h1.innerHTML = `<strong>Nazywam</strong> się ${name} i mam ${age}  lat`;
+
