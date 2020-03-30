f0c2c20ca142c5ac835ff365952aefc186065a9e
Michal Frelik <michal.frelik1@gmail.com>
2020-03-30 20:43:08 +0200

    Lesson: JS logiv, JS function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Siema');
 const name = 'Michał Frelik';
-const age = 26;
+const age = 80;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age}  lat`);
@@ -16,4 +16,44 @@ console.log(day__title__js);
 
 console.log(day__title__js.innerHTML);
 
-day__title__js.innerHTML = 'DOM = Document Object Model - wpisane JS-em'
+day__title__js.innerHTML = 'DOM = Document Object Model - wpisane JS-em';
+
+if ('javascript' != 'java') {
+    console.log('to prawda!');
+};
+
+if (age < 20) {
+    console.log ('Masz mniej niż 20 lat');
+}else if ((age >= 20) && (age <=30)) {
+    console.log ('Masz więcej niż 20 lat ale mniej niż 30 lat');
+}else {
+    console.log ('jesteś 31+')
+}
+
+switch (age) {
+    case 20:
+        console.log('masz równo 20 lat');
+    break;
+    case 30:
+        console.log('masz równo 30 lat');
+    break;
+    default:
+        console.log(`masz ${age} lat`);
+    break;
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log (amIOld);
+
+function calculate(x) {
+    x = x + 3;
+    console.log(`tradycujnie ${x}`);
+    return x*7;
+}
+
+console.log(calculate(2));
+
+const calculateFat = x => (x+3)*7; 
+
+console.log(calculateFat(2));
+
