910c04a1962fef8c107c37b863179127a5f269a6
Michal Frelik <michal.frelik1@gmail.com>
2020-04-14 14:44:25 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log(age);
 console.log(`Nazywam się ${name} i mam ${age}  lat`);
 
 const welcome = (name, age) => {
-    console.log(`Witaj ${name}, masz ${age} lat!`);
+	console.log(`Witaj ${name}, masz ${age} lat!`);
 };
 
 welcome(name, age);
@@ -16,25 +16,24 @@ const button = document.querySelector('.header__button--js');
 console.log(button);
 
 
-
 const navigationSwitcher = document.querySelector('.navigation__switcher--js');
 
 navigationSwitcher.addEventListener('click', (e) => {
-    const navigationList = document.querySelector('.navigation__list--js');
-    navigationList.classList.toggle('navigation__list--visible');
-    navigationSwitcher.innerHTML = 'XXX';
+	const navigationList = document.querySelector('.navigation__list--js');
+	navigationList.classList.toggle('navigation__list--visible');
+	navigationSwitcher.innerHTML = 'XXX';
 });
+
+
 
 button.addEventListener('click', (e) => {
-    const header = document.querySelector('.header__title--js')
-    header.innerHTML = '!! Pierwszy button !!';
-    header.classList.toggle('header__title--red');
-    if (header.classList.contains('header__title--red')) {
-        console.log('jest klasa');
-    } else {
-        console.log('brak klasy');
-    }
+	const header = document.querySelector('.header__title--js')
+	header.innerHTML = '!! Pierwszy button !!';
+	header.classList.toggle('header__title--red');
+	if (header.classList.contains('header__title--red')) {
+		console.log('jest klasa');
+	} else {
+		console.log('brak klasy');
+	}
 });
-
-
 
